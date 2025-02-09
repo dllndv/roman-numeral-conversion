d756fa55335c87b6f4d21da5c9dfd777a5b89d1e
Dallen Loder <dallenloder@Dallens-MacBook-Pro.local>
2025-02-08 21:59:52 -0700

    added algorithm to convert numbers to roman numerals

--- conversion-backend/app/numeral-conversion.ts
+++ conversion-backend/app/numeral-conversion.ts
@@ -1,5 +1,26 @@
 const convertNumber = (numToConvert: number) => {
-  return `here's the number you passed in: ${numToConvert}`;
+  enum digits {
+    ones,
+    tens,
+    huns,
+    thou,
+  }
+
+  const numeralsMap = new Map([
+    [digits.ones, ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]],
+    [digits.tens, ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]],
+    [digits.huns, ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]],
+    [digits.thou, ["", "M", "MM", "MMM"]],
+  ]);
+
+  const numString = numToConvert.toString();
+  const numReverseArray = numString.split("").reverse();
+
+  const romanNumerals = numReverseArray.map(
+    (digit, index) => numeralsMap.get(index)![+digit]
+  );
+
+  return romanNumerals.reverse().join("");
 };
 
 export { convertNumber };

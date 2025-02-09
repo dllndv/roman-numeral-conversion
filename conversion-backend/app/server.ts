56d85c1f63c83db66793584896da48612df2c8e5
Dallen Loder <dallenloder@Dallens-MacBook-Pro.local>
2025-02-08 22:55:20 -0700

    add route for roman numeral conversion

--- conversion-backend/app/server.ts
+++ conversion-backend/app/server.ts
@@ -1,10 +1,26 @@
 import { Application, Router, oakCors } from "../deps.ts";
+import { convertNumber } from "./numeral-conversion.ts";
 
 const router = new Router();
 
 const appServer = () => {
   router.get("/", (ctx) => {
     ctx.response.body = "Hello World!";
+  });
+  router.get("/romannumeral", (ctx) => {
+    const queryParams = ctx.request.url.searchParams;
+    const query = queryParams.get("query");
+
+    if (!query) {
+      ctx.response.status = 400;
+      ctx.response.body = { error: "query is required" };
+      return;
+    }
+    const romanNumeral = convertNumber(+query);
+    ctx.response.body = {
+      message: `Converted ${query} to roman numerals`,
+      data: romanNumeral,
+    };
   });
 
   const app = new Application();

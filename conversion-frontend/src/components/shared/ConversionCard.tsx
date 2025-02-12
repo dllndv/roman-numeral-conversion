416dfcbca4d26088b6250d870fc59e421cbd61d8
Dallen Loder <dallenloder@Dallens-MacBook-Pro.local>
2025-02-11 18:00:17 -0700

    add toast with message from backend

--- conversion-frontend/src/components/shared/ConversionCard.tsx
+++ conversion-frontend/src/components/shared/ConversionCard.tsx
@@ -1,4 +1,5 @@
 import { Form, NumberField, Button } from "@adobe/react-spectrum";
+import { ToastContainer, ToastQueue } from "@react-spectrum/toast";
 import { useState } from "react";
 import { styled } from "styled-components";
 
@@ -30,41 +31,45 @@ const ConversionCard = ({
     event.preventDefault();
     const conversionResult = await conversionFunction(numToConvert);
     setNumberConvertResult(conversionResult.data);
+    ToastQueue.positive(conversionResult.message, { timeout: 5000 });
   };
 
   return (
-    <CardContainer>
-      <h2>{title}</h2>
-      <Form
-        validationBehavior="native"
-        maxWidth="size-3600"
-        onSubmit={handleSubmit}
-        justifySelf={"left"}
-      >
-        <NumberField
-          data-testid="number-field"
-          label="Number to convert"
-          name="numToConvert"
-          minValue={minimumValue}
-          maxValue={maximumValue}
-          description={`Number must be between ${minimumValue} and ${maximumValue}`}
-          value={numToConvert}
-          onChange={setNumToConvert}
-          isRequired
-          errorMessage={({ validationDetails }) =>
-            validationDetails.valueMissing
-              ? "You must input a number to convert"
-              : ""
-          }
-        />
-        <Button type="submit" variant="primary">
-          {buttonLabel}
-        </Button>
-      </Form>
-      <p>
-        {outputLabel}: {numberConvertResult}
-      </p>
-    </CardContainer>
+    <>
+      <CardContainer>
+        <h2>{title}</h2>
+        <Form
+          validationBehavior="native"
+          maxWidth="size-3600"
+          onSubmit={handleSubmit}
+          justifySelf={"left"}
+        >
+          <NumberField
+            data-testid="number-field"
+            label="Number to convert"
+            name="numToConvert"
+            minValue={minimumValue}
+            maxValue={maximumValue}
+            description={`Number must be between ${minimumValue} and ${maximumValue}`}
+            value={numToConvert}
+            onChange={setNumToConvert}
+            isRequired
+            errorMessage={({ validationDetails }) =>
+              validationDetails.valueMissing
+                ? "You must input a number to convert"
+                : ""
+            }
+          />
+          <Button type="submit" variant="primary">
+            {buttonLabel}
+          </Button>
+        </Form>
+        <p>
+          {outputLabel}: {numberConvertResult}
+        </p>
+      </CardContainer>
+      <ToastContainer />
+    </>
   );
 };
 
